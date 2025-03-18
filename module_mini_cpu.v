de9bfd49f9b62aeeccf51ed0477cae4d082fd683
Marina Rodas <122946394+marsales@users.noreply.github.com>
2025-03-18 14:51:00 -0300

    Update module_mini_cpu.v

--- module_mini_cpu.v
+++ module_mini_cpu.v
@@ -6,10 +6,13 @@ module module_mini_cpu (
     input [5:0] Imm
     input [3:0] dest,
     input clk,
-    input ligar, enviar
+    input ligar, enviar,
+    input we,
+
+    output [15:0] valorFinal
 );
 
-    // OPERAÇÕES
+    // Operações
     parameter LOAD = 3'b000,
               ADD = 3'b001,
               ADDI = 3'b010,
@@ -20,26 +23,103 @@ module module_mini_cpu (
               DISPLAY = 3'b111;
 
     reg [2:0] state;
+    reg [15:0] dadosRAM [15:0];
+    reg [15:0] leituraRAM;
+    reg mostrarNumsLCD = 0 // 0 - Não mostrar, 1 - Mostrar
+
+    // RAM
+    memory ram (
+        data.(dadosRAM),
+        addr1.(src1),
+        addr2.(src2),
+        dest.(dest),
+        we.(we),
+        clk.(clk),
+        q.(leituraRAM)
+    );
+
+    // ULA
+    module_alu ula (
+        opcode.(opcode),
+        valor1.(dadosRAM[src1]),
+        valor2.(dadosRAM[src2]),
+        clk.(clk),
+        saida.(valorFinal)
+    );
+
+    // Inicialização
+    initial begin
+        we <= 0;
+        mostrarNumsLCD <= 0;
+    end
 
     // Primeiro always -> Estado próximo
-    always @ (posedge clk) begin
+    always @ (posedge enviar) begin
 
+        // Mudar o estado baseado no opcode
         case(opcode)
 
-            LOAD: state <= LOAD;
-            ADD: state <= ADD;
-            ADDI: state <= ADDI;
-            SUB: state <= SUB;
-            SUBI: state <= SUBI;
-            MUL: state <= MUL;
-            CLEAR: state <= CLEAR;
-            DISPLAY: state <= DISPLAY;
+            LOAD: begin 
+                state <= LOAD;
+                we <= 1;
+                mostrarNumsLCD <= 1;
+            end
+            ADD: begin 
+                state <= ADD;
+                we <= 1;
+                mostrarNumsLCD <= 1;
+            end
+            ADDI: begin
+                state <= ADDI;
+                we <= 1;
+                mostrarNumsLCD <= 1;
+            end
+            SUB: begin 
+                state <= SUB;
+                we <= 1;
+                mostrarNumsLCD <= 1;
+            end
+            SUBI: begin 
+                state <= SUBI;
+                we <= 1;
+                mostrarNumsLCD <= 1;
+            end
+            MUL: begin 
+                state <= MUL;
+                we <= 1;
+                mostrarNumsLCD <= 1;
+            end
+            CLEAR: begin 
+                state <= CLEAR;
+                we <= 1;
+                mostrarNumsLCD <= 0;
+            end
+            DISPLAY: begin 
+                state <= DISPLAY;
+                we <= 0;
+                mostrarNumsLCD <= 1;
+            end
 
         endcase
 
     end
 
     // Segundo always -> Saída
-    
+    always @ (posedge clk) begin
 
+        case (state)
+
+            LOAD:
+            ADD:
+            ADDI:
+            SUB:
+            SUBI:
+            MUL:
+            CLEAR:
+            DISPLAY:
+
+        endcase
+
+    end
+    
 endmodule
