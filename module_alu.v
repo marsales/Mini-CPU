d5b275b8c943aadd76461a1ce2a1c30c0bee95ad
Marina Rodas <122946394+marsales@users.noreply.github.com>
2025-03-18 14:51:28 -0300

    Update module_alu.v

--- module_alu.v
+++ module_alu.v
@@ -1,11 +1,14 @@
 module module_alu (
     input [2:0] opcode,
     input [15:0] valor1,
-    input [15:0] valor2
-    input clk
+    input [15:0] valor2,
+    input sinalImm,
+    input [5:0] Imm,
+    input clk,
+    output [15:0] saida
 );
 
-    // OPERAÇÕES DA CPU
+    // OPERAÇÕES
     parameter LOAD = 3'b000,
               ADD = 3'b001,
               ADDI = 3'b010,
@@ -15,28 +18,46 @@ module module_alu (
               CLEAR = 3'b110,
               DISPLAY = 3'b111;
 
-    // OPERAÇÕES DA ULA
-    parameter SOMA = 2'00,
-              SUBTRACAO = 2'b01,
-              MULTIPLICACAO = 2'b10,
-              NADA = 2'b11;
-
     reg [2:0] state;
 
     // Primeiro always -> Estado próximo
     always @ (posedge clk) begin
 
         case(opcode)
-            ADD: state <= SOMA;
-            ADDI: state <= SOMA;
-            SUB: state <= SUBTRACAO;
-            SUBI: state <= SUBTRACAO;
-            MUL: state <= MULTIPLICACAO;
-            default: state <= NADA;
+            LOAD: state <= LOAD;
+            ADD: state <= ADD;
+            ADDI: state <= ADDI;
+            SUB: state <= SUB;
+            SUBI: state <= SUBI;
+            MUL: state <= MUL;
+            CLEAR: state <= CLEAR;
         endcase
         
     end
 
     // Segundo always -> Saída
+    always @ (posedge clk) begin
+
+        case(state)
+            LOAD: saida <= Imm;
+            ADD: saida <= valor1 + valor2;
+            ADDI: begin
+                if (sinalImm) saida <= valor1 - Imm;
+                else saida <= valor1 + Imm;
+            end
+            SUB: saida <= valor1 - valor2;
+            SUBI: begin
+                if (sinalImm) saida <= valor1 + Imm;
+                else saida <= valor1 - Imm;
+            end
+            MUL: begin
+                if (sinalImm) saida <= -1 * valor1 * Imm;
+                else saida <= valor1 * Imm;
+            end
+            CLEAR: saida <= 0;
+            DISPLAY: saida <= valor1;
+        endcase
+
+    end
 
 endmodule
